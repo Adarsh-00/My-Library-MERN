21f1f9079f54f3ed85e51dbc0f553c0c7e88baf8
Adarsh-00 <134307234+Adarsh-00@users.noreply.github.com>
2025-08-27 11:33:42 +0530

    Add files via upload

---
+++ Bookstore-Backend/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Books = require('./model/bookModel');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://127.0.0.1:27017/library')
+    .then(()=> console.log('db connected successfull'));
+
+
+app.get('/', (req, res) => {
+    res.send('<h1> working? </h1>')
+});
+
+app.get('/get-books', async (req, res) => {
+    try {
+        const books = await Books.find();
+        if(books.length < 1) {
+            res.status(404).send("<h1> No Record Found </h1>");
+        }
+        res.send(books);
+    } catch (error) {
+        res.send(`<h1> ${error.message} </h1>`);
+    }
+});
+
+app.get('/get-books/:id', async (req, res) => {
+    try {
+        const book = await Books.findById(req.params.id);
+        if(book.length < 1) {
+            res.status(404).send("<h1> No Record Found </h1>");
+        }
+        res.send(book);
+    } catch (error) {
+        res.send(`<h1> ${error.message} </h1>`);
+    }
+});
+
+app.post('/add-book', async (req, res) => {
+    const newbook = new Books(req.body);
+    await newbook.save();
+    res.json(newbook);
+
+});
+
+app.put('/update-book/:id', async(req, res) => {
+    const updatedBook = await Books.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description,
+        published: req.body.published
+    }, {new: true});
+
+    res.send(updatedBook);
+
+});
+
+app.delete('/remove-book/:id', (req, res) => {
+    Books.findByIdAndDelete(req.params.id)
+    .then(() => res.send("book deleted successfully"))
+    .catch((err)=> {
+        res.send(err.message);
+    });
+});
+
+app.listen(8080, ()=> {
+    console.log('listing...');
+});
